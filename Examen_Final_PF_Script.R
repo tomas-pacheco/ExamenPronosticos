da894803b40e9ec01cb29df581010996b2dbd9db
AbigailRiquelme <82173618+AbigailRiquelme@users.noreply.github.com>
2021-06-16 10:10:49 -0300

    Update Examen_Final_PF_Script.R
    pronosticos

--- Examen_Final_PF_Script.R
+++ Examen_Final_PF_Script.R
@@ -581,15 +581,15 @@ reservas.est <- diff(data1[,6])
 
 dolar.est <- diff(data1[,9])
 
-data.diff<-cbind(data1[-1,-c(1,2,6,9,15)], reservas.est, dolar.est)
+data.diff<-cbind(data1[-1,-c(1,2,6,9,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)], reservas.est, dolar.est)
 
 h<-1
 
 for(i in 1:58){ 
   temp2<-window(data.diff, start = c(2019,12), end = 2020.195 + (i-1)/365)
-  f5 <- VAR(temp2, p = 1, type = "trend", season = 4)
-  forecast1<- predict(f5, h= h)
-  pr.f.h1[i,5] <- forecast1$fcst$data1..1...c.1..2..6..9..15...sentsmooth[h]
+  f5 <- VAR(temp2, p = 6, type = "trend", season = 4)
+  forecast1<- forecast(f5)
+  pr.f.h1[i,5] <- forecast1$forecast$data1..1...c.1..2..6..9..15..17..18..19..20..21..22..23..24...sentsmooth$mean[h]
 }
 
 
@@ -685,9 +685,7 @@ reservas.est <- diff(data1[,6])
 
 dolar.est <- diff(data1[,9])
 
-data.diff<-cbind(data1[-1,-c(1,2,6,9,15)], reservas.est, dolar.est)
-
-library(forecast)
+data.diff<-cbind(data1[-1,-c(1,2,6,9,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)], reservas.est, dolar.est)
 
 h<-1
 
@@ -697,8 +695,127 @@ for(i in 1:58){
   b<-a$selection
   c<-b[1]
   f5 <- VAR(temp2, p = c, type = "trend", season = 4)
-  forecast1<- forecast(f5, h= h)
-  pr.rec.h1[i,5] <- forecast1$forecast$data1..1...c.1..2..6..9..15...sentsmooth$mean[h]
+  forecast1<- forecast(f5)
+  pr.rec.h1[i,5] <- forecast1$forecast$data1..1...c.1..2..6..9..15..17..18..19..20..21..22..23..24...sentsmooth$mean[h]
 }
+
+
+# Gráfico con los pronósticos recursivos y h=1 
+
+autoplot(ts.union(out.of.sample[,3], pr.rec.h1[,1], pr.rec.h1[,2],pr.rec.h1[,3],pr.rec.h1[,4],pr.rec.h1[,5]), size = 1.3) + 
+  scale_color_manual(name = "", labels = c("Actual", "ARIMA", "ARIMAX","ETS","ADL","VAR"),
+                     values = c("black", "sienna2", "palegreen3","#00AFBB", colores[2], colores[1])) +
+  ggtitle("Pronósticos recursivos con h = 1") + 
+  xlab("Tiempo") + ylab("Sentimiento Alberto")+
+  theme_minimal() +  
+  theme(legend.position = c(0.1,0.80), plot.title = element_text(hjust = 0.5))
+
+
+
+# ESQUEMA ROLLING 
+
+
+data1 <- ts(data, frequency = 365, start = c(2019,12))
+
+pr.rol.h1 <- ts(matrix(0, 58, 5), frequency = 365, start=c(2020,11))
+colnames(pr.rec.h1) <- c("ARIMA", "ARIMAX", "ADL", "ETS","VAR")
+h<-1
+for(i in 1:58){
+  temp<-window(data1[,3], start = 2019.030 + (i-1)/365, end = 2020.195 + (i-1)/365)
+  temp2 <-window(data1[,4:17], start = c(2019,12), end = 2020.195 + (i-1)/365)
+  
+  # ARIMA 
+  f1 <- auto.arima(temp)
+  forecast <- forecast(f1,h = h)
+  pr.rol.h1[i,1] <- forecast$mean[h]
+  
+  #ARIMAX
+  f2 <- Arima(temp,model=f1, newxreg=temp2)
+  forecast2 <- forecast(f2$fitted,h=h)
+  pr.rol.h1[i,2] <- forecast2$mean[h]
+  
+  #ETS 
+  f3 <- ets(temp)
+  pre <- forecast(f1, n.ahead=h)
+  pr.rol.h1[i,3] <- pre$mean[h]
+  
+}
+
+
+#ADL 
+
+data_1.1<-ts(data_1,frequency = 365, start = c(2019,12))
+
+data_dum.1<-data_1.1[1:425,-c(1,2,15,30)]
+
+h<-1
+count <- 2
+for(i in 1:58){
+  temp2 <-window(data_1.1[,-c(1,2,30)], start = 2019.030 + (i-1)/365, end = 2020.195 + (i-1)/365)
+  colnames(temp2) <- c("sentsmooth","twfav", "twret", "reservasbcra", "tasaint", "basemon", "tcdolar", "casosarg",          
+                       "muertosarg", "vacunasarg", "maxtemp", "mintemp", "sentiment_trend_orig",
+                       "sent_trends", "muertes.arg.rel", "casos.arg.rel",     
+                       "mes01", "mes02", "mes03", "mes04", "mes05", "mes06",
+                       "mes07", "mes08", "mes09", "mes10", "mes11")
+  
+  # Actualizamos el orden del ADL
+  
+  order.adl.dl <- auto_ardl(sentsmooth ~ twfav + twret + reservasbcra + tasaint + basemon + tcdolar + 
+                              casosarg + muertosarg + vacunasarg + maxtemp + 
+                              mintemp + sent_trends + muertes.arg.rel + casos.arg.rel|
+                              mes01 + mes02 + mes03 +  mes04 + mes05 + mes06 +
+                              mes07 + mes08 + mes09 +  mes10 + mes11, 
+                            data = temp2, max_order = 5)
+  
+  adl.dl <- ardl(sentsmooth ~ twfav + twret + reservasbcra + tasaint + basemon + tcdolar + 
+                   casosarg + muertosarg + vacunasarg + maxtemp + 
+                   mintemp + sent_trends + muertes.arg.rel + casos.arg.rel|
+                   mes01 + mes02 + mes03 +  mes04 + mes05 + mes06 +
+                   mes07 + mes08 + mes09 +  mes10 + mes11, 
+                 data = temp2, order = as.vector(order.adl.dl$best_order))
+  
+  # Estimamos el ADL pero con la función dynlm
+  
+  adl<-adl.dl$full_formula
+  adl.1 <- dynlm(adl, data = adl.dl$data)
+  forecast2 <- predict(adl.1$fitted.values,h=1)
+  pr.rol.h1[i,4] <- forecast2$mean[h]
+  print(count) 
+  count = count + 1
+}
+
+
+# Realizamos los pronósticos con el VAR 
+
+# Construimos la serie diferenciando las variables que son I(1)
+
+reservas.est <- diff(data1[,6])
+
+dolar.est <- diff(data1[,9])
+
+data.diff<-cbind(data1[-1,-c(1,2,6,9,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)], reservas.est, dolar.est)
+
+h<-1
+
+for(i in 1:58){ 
+  temp2<-window(data.diff, start = 2019.033 + (i-1)/365, end = 2020.195 + (i-1)/365)
+  a<-VARselect(data.diff, lag.max = 13, type = "const")
+  b<-a$selection
+  c<-b[1]
+  f5 <- VAR(temp2, p = c, type = "trend", season = 4)
+  forecast1<- forecast(f5)
+  pr.rol.h1[i,5] <- forecast1$forecast$data1..1...c.1..2..6..9..15..17..18..19..20..21..22..23..24...sentsmooth$mean[h]
+}
+
+
+# Gráfico con los pronósticos rolling y h=1 
+
+autoplot(ts.union(out.of.sample[,3], pr.rol.h1[,1], pr.rol.h1[,2],pr.rol.h1[,3],pr.rol.h1[,4],pr.rol.h1[,5]), size = 1.3) + 
+  scale_color_manual(name = "", labels = c("Actual", "ARIMA", "ARIMAX","ETS","ADL","VAR"),
+                     values = c("black", "sienna2", "palegreen3","#00AFBB", colores[2], colores[1])) +
+  ggtitle("Pronósticos rolling con h = 1") + 
+  xlab("Tiempo") + ylab("Sentimiento Alberto")+
+  theme_minimal() +  
+  theme(legend.position = c(0.1,0.80), plot.title = element_text(hjust = 0.5))
 
 
